863de18f2aef1cdc91d57bbf471705b0ae000d36
Rohith M <rohithmerala@gmail.com>
2022-11-02 20:04:38 +0530

    fe

--- Pages/bottles.js
+++ Pages/bottles.js
@@ -3,8 +3,20 @@ img = "";
 function setup() {
   canvas = createCanvas(600, 600);
   canvas.position(440);
+  object_detector = ml5.objectDetector("cocossd", modelloaded);
 }
 
+function modelloaded() {
+  console.log("Model Has Loaded!!!");
+  object_detector.detect(img, gotresult);
+}
+function gotresult(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results);
+  }
+}
 function preload() {
   img = loadImage("/images/bottles.jpg");
 }
--- Pages/tv.js
+++ Pages/tv.js
@@ -3,6 +3,19 @@ img = "";
 function setup() {
   canvas = createCanvas(640, 420);
   canvas.center();
+  object_detector = ml5.objectDetector("cocossd", modelloaded);
+}
+
+function modelloaded() {
+  console.log("Model Has Loaded!!!");
+  object_detector.detect(img, gotresult);
+}
+function gotresult(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results);
+  }
 }
 
 function preload() {
